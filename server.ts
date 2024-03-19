c19f25b48eef01e15cb9fb842cc2f4614295c6d0
HenriqueGomes <henriquegomesjunqueira@gmail.com>
2024-03-18 21:23:22 -0300

    feat: added database connection

--- server.ts
+++ server.ts
@@ -1,14 +1,17 @@
-import express, { Express, Request, Response , Application } from 'express';
-import { MongoDatabase } from './db/connection'
+import express, { Application, Request, Response } from 'express';
+import { MongoDatabase } from './db/connection';
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+    res.send('Welcome to Express & TypeScript Server');
 });
 
-app.listen(port, async () => {
-    await MongoDatabase.connect()
-  console.log(`Server is Fire at http://localhost:${port}`);
-});
+
+MongoDatabase.connect().then(() => {
+    app.listen(port, async () => {
+        console.log(`Server is Fire at http://localhost:${port}`);
+    });
+}).catch((err) => console.log(err, "Ocorreu um erro ao tentar iniciar o banco de dados"))
+
