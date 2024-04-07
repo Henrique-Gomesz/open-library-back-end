b5da4237a74e8f4cfca4e14e578fed340a84b112
Henrique Gomes <henriquegomesjunqueira@gmail.com>
2024-04-07 00:39:49 -0300

    feaT: remove comments

--- observer/publicador.ts
+++ observer/publicador.ts
@@ -1,12 +1,7 @@
 import { Assinante } from "./assinantes";
 
 export interface Publicador {
-    // Attach an observer to the subject.
     inserirAssinante(observer: Assinante): void;
-
-    // Detach an observer from the subject.
     removerAssinante(observer: Assinante): void;
-
-    // Notify all observers about an event.
     notificar(mensage:string): void;
 }
